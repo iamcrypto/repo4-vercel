2318c0e26e4c08702dcd071bafb9a552e009e49c
iamlive <71422116+iamcrypto@users.noreply.github.com>
2024-11-21 19:05:39 +0530

    Update connectDB.js
    database connection added

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12745196',
-    password: 'HGYzlpDWe9',
-    database: 'sql12745196',
-    port: '3306'
+  host: 'dbtiran.cjakccac2nys.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Ajax24x7#365',
+  database: 'victory',
+  port: '3306' 
    
 });
 
